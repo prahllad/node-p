eee10458fb951ffa0007663add4247ffcf127b4f
Prahllad <royprahllad3@gmail.com>
2023-07-08 18:59:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
  const express = require('express');
  const app = express();
  app.get('',(req,res)=>{
-     res.send('hello world');
+     res.send('<h1>hello world</h1>');
  })
  var port = process.env.PORT || 3000;
 
